833438bad238bd58f6e3863d6b6fc1017ff499c3
Ravi S. Iyer <riyer02@gmail.com>
2025-10-15 22:00:30 +0530

    Fixed home page image vertical clipping on web by using stretch for beyond mobile width

--- components/HomeBlock.tsx
+++ components/HomeBlock.tsx
@@ -1,7 +1,7 @@
 'use dom';
 import { HomeInfo } from '@/types/interfaces';
 import { BlocksRenderer } from '@strapi/blocks-react-renderer';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, Dimensions } from 'react-native';
 import '@/global.css';
 import { Link } from 'expo-router';
 import { FontAwesome5 } from '@expo/vector-icons';
@@ -16,12 +16,23 @@ export default function HomeBlock({
   dom: import('expo/dom').DOMProps;
 }) {
   const blockContent = homeInfo?.content;
-
   // blockContent && console.log('blockContent', blockContent);
+
+  const windowWidth = Dimensions.get('window').width;
+  const isMobileWidth = windowWidth < 768;
+
+  console.log('windowWidth', windowWidth, 'isMobileWidth', isMobileWidth);
   return (
     <Animated.View className="w-screen pb-14" entering={FadeIn.duration(200).easing(Easing.ease)}>
-      <Image source={{ uri: homeInfo?.image }} className="w-screen h-40" />
-      <View className={`w-screen p-4 ${colorScheme === 'dark' ? 'text-white' : ''}`}>
+      {/* <Image source={{ uri: homeInfo?.image }} className="w-screen h-40" /> */}
+      {/* <Image source={{ uri: homeInfo?.image }} className="w-screen h-40" resizeMode="contain" /> */}
+       <Image 
+          source={{ uri: homeInfo?.image }} 
+            // className={isMobileWidth ? 'w-full aspect-video' : 'w-full h-[40vh]'}
+            className={isMobileWidth ? 'w-full aspect-video' : 'w-full h-40'}
+            resizeMode={isMobileWidth ? 'contain' : 'stretch'}
+        />
+     <View className={`w-screen p-4 ${colorScheme === 'dark' ? 'text-white' : ''}`}>
           {blockContent && <BlocksRenderer content={blockContent}/>}
       </View>
       <Link href="/courses" asChild>
