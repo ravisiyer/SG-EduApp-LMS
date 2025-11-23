0cb5ea309da2df9cb084345f1560ed776fa52c24
Ravi S. Iyer <riyer02@gmail.com>
2025-11-23 21:54:32 +0530

    Clerk and dummy sign ins are enabled. For web, it is working

--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,7 @@
 import { Text, View, Pressable, ActivityIndicator, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useState } from 'react';
-// import { useSSO } from '@clerk/clerk-expo';
+import { useSSO } from '@clerk/clerk-expo';
 import { useStrapi } from '@/providers/StrapiProvider';
 import { randomUUID } from 'expo-crypto';
 import { router } from 'expo-router';
@@ -9,50 +9,50 @@ import { useDummyAuth } from '@/providers/DummyAuthContext';
 
 export default function Index() {
   const [loading, setLoading] = useState(false);
-  // const { startSSOFlow } = useSSO();
+  const { startSSOFlow } = useSSO();
   const { createUser } = useStrapi();
   const { dummySignIn } = useDummyAuth();
-  // const handleSignInWithSSO = async (strategy: 'oauth_google' | 'oauth_apple') => {
-  //   try {
-  //     const { createdSessionId, setActive, signUp, signIn } = await startSSOFlow({ strategy });
+  const handleSignInWithSSO = async (strategy: 'oauth_google' | 'oauth_apple') => {
+    try {
+      const { createdSessionId, setActive, signUp, signIn } = await startSSOFlow({ strategy });
 
-  //     // console.log(signUp, signIn);
+      // console.log(signUp, signIn);
 
-  //     if (createdSessionId) {
-  //       await setActive!({ session: createdSessionId });
+      if (createdSessionId) {
+        await setActive!({ session: createdSessionId });
 
-  //       // Only create Strapi user if this is a new signup
-  //       // In debugging, I found that if user is only signing in, signUp is not null
-  //       // but signUp.id is undefined and signUp.status (also _status) is null
-  //       // https://clerk.com/docs/reference/javascript/sign-up#properties states that 
-  //       // status 'complete' means:
-  //       // The user has been created and the custom flow can proceed to setActive() to create session.
-  //       //
-  //       // I also had to change sign up mode in Clerk dashboard from waitlist to public.
-  //       // After that, below code worked by differentiating correctly between new signup and existing user signin.
-  //       // But this approach does not seem to be documented in Clerk docs.
-  //       // The official approach seems to be more complicated which I want to skip for this tutorial.
-  //       // For a production app, I will have to use an official approach provided in Clerk docs.
-  //       if (signUp && signUp.status === 'complete') {
-  //         const email = signUp.emailAddress;
-  //         const username = email;
-  //         const password = randomUUID();
-  //         const id = signUp.createdUserId;
+        // Only create Strapi user if this is a new signup
+        // In debugging, I found that if user is only signing in, signUp is not null
+        // but signUp.id is undefined and signUp.status (also _status) is null
+        // https://clerk.com/docs/reference/javascript/sign-up#properties states that 
+        // status 'complete' means:
+        // The user has been created and the custom flow can proceed to setActive() to create session.
+        //
+        // I also had to change sign up mode in Clerk dashboard from waitlist to public.
+        // After that, below code worked by differentiating correctly between new signup and existing user signin.
+        // But this approach does not seem to be documented in Clerk docs.
+        // The official approach seems to be more complicated which I want to skip for this tutorial.
+        // For a production app, I will have to use an official approach provided in Clerk docs.
+        if (signUp && signUp.status === 'complete') {
+          const email = signUp.emailAddress;
+          const username = email;
+          const password = randomUUID();
+          const id = signUp.createdUserId;
 
-  //         // console.log(email, username, password, id);
+          // console.log(email, username, password, id);
 
-  //         if (!email || !username || !password || !id) {
-  //           throw new Error('Missing required fields for new user signup.');
-  //         }
+          if (!email || !username || !password || !id) {
+            throw new Error('Missing required fields for new user signup.');
+          }
 
-  //         const strapiUser = { email, username, password, clerkId: id };
-  //         await createUser(strapiUser);
-  //       }
-  //     }
-  //   } catch (err) {
-  //     console.log('SSO flow error:', err);
-  //   }
-  // };
+          const strapiUser = { email, username, password, clerkId: id };
+          await createUser(strapiUser);
+        }
+      }
+    } catch (err) {
+      console.log('SSO flow error:', err);
+    }
+  };
 
   const handleDummySignIn = () => {
     dummySignIn();
@@ -86,7 +86,7 @@ export default function Index() {
               <Text className="text-black text-center font-semibold ml-2">Continue with Apple</Text>
             </Pressable> */}
 
-            {/* <Pressable
+            <Pressable
               className="w-full flex-row justify-center items-center bg-white py-3 rounded-lg hover:cursor-pointer hover:bg-gray-800 duration-300"
               onPress={() => handleSignInWithSSO('oauth_google')}
               >
@@ -94,7 +94,7 @@ export default function Index() {
               <Text className="text-black text-center font-semibold ml-2">
                 Continue with Google
               </Text>
-            </Pressable> */}
+            </Pressable>
 
             <Pressable
               className="w-full flex-row justify-center items-center bg-white py-3 rounded-lg hover:cursor-pointer hover:bg-gray-800 duration-300"
